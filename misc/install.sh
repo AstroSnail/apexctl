4809118be46b279b5f0600930f352fa50cca7581
AstroSnail <astrosnail@protonmail.com>
2020-05-19 08:06:35 +0000

    Fix up install.sh a bit with shellcheck

--- misc/install.sh
+++ misc/install.sh
@@ -1,27 +1,27 @@
 #!/bin/sh -eu
 
-: ${BINDIR:=/usr/local/sbin}
-: ${UDEVHWDBDIR:=/etc/udev/hwdb.d}
-: ${UDEVRULESDIR:=/etc/udev/rules.d}
+: "${BINDIR:=/usr/local/sbin}"
+: "${UDEVHWDBDIR:=/etc/udev/hwdb.d}"
+: "${UDEVRULESDIR:=/etc/udev/rules.d}"
 
-: ${USEXORG:=}
-: ${DISPLAY:=}
-: ${XORGCONFDIR:=/etc/X11/xorg.conf.d}
+: "${USEXORG:=}"
+: "${DISPLAY:=}"
+: "${XORGCONFDIR:=/etc/X11/xorg.conf.d}"
 
 apex_install () {
-	mkdir --parents "$BINDIR"
-	install --mode=0755 apexctl "$BINDIR"/apexctl
+	mkdir -p "${BINDIR}"
+	install -m0755 apexctl "${BINDIR}/apexctl"
 
-	mkdir --parents "$UDEVHWDBDIR"
-	install --mode=0644 config/default/00-apex.hwdb "$UDEVHWDBDIR"/90-apex.hwdb
+	mkdir -p "${UDEVHWDBDIR}"
+	install -m0644 config/default/00-apex.hwdb "${UDEVHWDBDIR}/90-apex.hwdb"
 
-	mkdir --parents "$UDEVRULESDIR"
-	install --mode=0644 config/default/00-apexctl.rules "$UDEVRULESDIR"/90-apexctl.rules
+	mkdir -p "${UDEVRULESDIR}"
+	install -m0644 config/default/00-apexctl.rules "${UDEVRULESDIR}/90-apexctl.rules"
 
-	if [ "$USEXORG" = "y" ]
+	if [ "${USEXORG}" = "y" ]
 	then
-		mkdir --parents "$XORGCONFDIR"
-		install --mode=0644 config/default/00-apex.conf "$XORGCONFDIR"/90-apex.conf
+		mkdir -p "${XORGCONFDIR}"
+		install -m0644 config/default/00-apex.conf "${XORGCONFDIR}/90-apex.conf"
 	fi
 
 	udevadm hwdb --update
@@ -29,29 +29,29 @@ apex_install () {
 }
 
 apex_uninstall () {
-	rm --force "$BINDIR"/apexctl
-	rm --force "$UDEVHWDBDIR"/90-apex.hwdb
-	rm --force "$UDEVRULESDIR"/90-apexctl.rules
-	rm --force "$XORGCONFDIR"/90-apex.conf
+	rm -f "${BINDIR}/apexctl"
+	rm -f "${UDEVHWDBDIR}/90-apex.hwdb"
+	rm -f "${UDEVRULESDIR}/90-apexctl.rules"
+	rm -f "${XORGCONFDIR}/90-apex.conf"
 
 	udevadm hwdb --update
 	udevadm trigger
 }
 
-case ":$PATH:" in
-	(*:"$BINDIR":*)
+case ":${PATH}:" in
+	(*:"${BINDIR}":*)
 		;;
 	(*)
-		echo 'It appears `'"$BINDIR"\'' is not in your $PATH'
-		echo 'Either add it to your $PATH in your shell profile,'
-		echo 'or set $BINDIR to somewhere in your $PATH when you run this installer.'
-		echo 'Example: sudo env BINDIR=/usr/sbin make install'
+		echo "It appears \`${BINDIR}' is not in your \$PATH"
+		echo "Either add it to your \$PATH in your shell profile,"
+		echo "or set \$BINDIR to somewhere in your \$PATH when you run this installer."
+		echo "Example: sudo env BINDIR=/usr/sbin make install"
 		exit 1
 esac
 
 action=apex_install
 if [ "${1:-}" = -u ]; then action=apex_uninstall; shift; fi
-if [ -z "$USEXORG" ] && [ -n "$DISPLAY" ]; then USEXORG=y; fi
+if [ -z "${USEXORG}" ] && [ -n "${DISPLAY}" ]; then USEXORG=y; fi
 
 set -x
-$action
+"${action}"
