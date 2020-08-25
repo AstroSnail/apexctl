bf66819330918ce9089dcd0fb786abd9dc49a049
AstroSnail <astrosnail@protonmail.com>
2020-08-25 11:37:22 +0000

    Add default.nix for easy testing on NixOS

---
+++ default.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> { } }: pkgs.stdenv.mkDerivation {
+  name = "apexctl";
+  src = builtins.path {
+    name = "apexctl";
+    path = ./.;
+  };
+  buildInputs = [ pkgs.hidapi ];
+  preBuild = ''
+    buildFlagsArray+=(CC=gcc)
+    buildFlagsArray+=(CPPLIBS="''${NIX_CFLAGS_COMPILE}")
+    buildFlagsArray+=(LDFLAGS="-Wl,$(tr -s ' ' , <<<"''${NIX_LDFLAGS}")")
+    buildFlagsArray+=(LIBS=-lhidapi-libusb)
+    buildFlagsArray+=(HIDAPI_LONG_INCLUDE=1)
+  '';
+  preInstall = ''
+    export BINDIR="$out/bin"
+    export UDEVHWDBDIR="$out/etc/udev/hwdb.d"
+    export UDEVRULESDIR="$out/etc/udev/rules.d"
+    export USEXORG=y
+    export XORGCONFDIR="$out/etc/X11/xorg.conf.d"
+    export XKBDIR="$out/etc/X11/xkb"
+    export IHAVEANALLTERRAINVEHICLE=y
+    export UDEVRELOAD=n
+  '';
+  installTargets = "install-advanced";
+}
