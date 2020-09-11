ed1cef0dff516e5c4c03d55ba9f6bc49fd30de8f
AstroSnail <astrosnail@protonmail.com>
2020-09-11 10:31:41 +0000

    Use flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A flake for building ApexCtl";
+  outputs = { self, nixpkgs }:
+    let
+      apexctl = { pkgs }: {
+        name = "apexctl";
+        src = self;
+        buildInputs = [ pkgs.hidapi ];
+        preBuild = ''
+          buildFlagsArray+=(CC=gcc)
+          buildFlagsArray+=(CPPLIBS="''${NIX_CFLAGS_COMPILE}")
+          buildFlagsArray+=(LDFLAGS=-Wl,"$(tr -s ' ' , <<<"''${NIX_LDFLAGS}")")
+          buildFlagsArray+=(LDLIBS=-lhidapi-libusb)
+          buildFlagsArray+=(HIDAPI_LONG_INCLUDE=1)
+          export BINDIR="$out/bin"
+        '';
+        preInstall = ''
+          export BINDIR="$out/bin"
+          export UDEVHWDBDIR="$out/etc/udev/hwdb.d"
+          export UDEVRULESDIR="$out/etc/udev/rules.d"
+          export USEXORG=y
+          export XORGCONFDIR="$out/etc/X11/xorg.conf.d"
+          export IHAVEANALLTERRAINVEHICLE=y
+          export UDEVRELOAD=n
+        '';
+      };
+      pkgs-x86_64-linux = import nixpkgs { system = "x86_64-linux"; };
+    in {
+      defaultPackage.x86_64-linux = pkgs-x86_64-linux.stdenv.mkDerivation
+        (apexctl { pkgs = pkgs-x86_64-linux; });
+    };
+}
