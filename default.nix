f909cb606f2f799d602466abb099ce8a745c504b
AstroSnail <astrosnail@protonmail.com>
2020-09-20 12:27:00 +0000

    Add advanced install to flake

--- default.nix
+++ default.nix
@@ -1,18 +1,20 @@
 { self ? builtins.path {
   name = "apexctl";
   path = ./.;
-}, stdenv, hidapi }:
+}, stdenv, hidapi, advanced ? false }:
 stdenv.mkDerivation {
   name = "apexctl";
   src = self;
   buildInputs = [ hidapi ];
   preBuild = ''
+    export BINDIR="$out/bin"
     buildFlagsArray+=(CC=gcc)
     buildFlagsArray+=(CPPLIBS="''${NIX_CFLAGS_COMPILE}")
     buildFlagsArray+=(LDFLAGS=-Wl,"$(tr -s ' ' , <<<"''${NIX_LDFLAGS}")")
     buildFlagsArray+=(LDLIBS=-lhidapi-libusb)
     buildFlagsArray+=(HIDAPI_LONG_INCLUDE=1)
-    export BINDIR="$out/bin"
+  '' + stdenv.lib.optionalString advanced ''
+    buildFlagsArray+=(all-advanced)
   '';
   preInstall = ''
     export BINDIR="$out/bin"
@@ -22,5 +24,13 @@ stdenv.mkDerivation {
     export XORGCONFDIR="$out/etc/X11/xorg.conf.d"
     export IHAVEANALLTERRAINVEHICLE=y
     export UDEVRELOAD=n
+  '' + stdenv.lib.optionalString advanced ''
+    export XKBDIR="$out/etc/X11/xkb"
   '';
+  installTargets = if advanced then "install-advanced" else "install";
+  meta = {
+    description = "A program to control SteelSeries Apex keyboards";
+    license = stdenv.lib.licenses.asl20;
+    platforms = [ "x86_64-linux" ];
+  };
 }
--- flake.nix
+++ flake.nix
@@ -3,9 +3,18 @@
   outputs = { self, nixpkgs }:
     let supportedSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
     in {
-      defaultPackage = supportedSystems (system:
-        nixpkgs.legacyPackages."${system}".callPackage ./default.nix {
-          inherit self;
+      packages = supportedSystems (system:
+        let
+          callApexctl =
+            nixpkgs.legacyPackages."${system}".callPackage ./default.nix;
+        in {
+          apexctl = callApexctl { inherit self; };
+          apexctl-advanced = callApexctl {
+            inherit self;
+            advanced = true;
+          };
         });
+      defaultPackage =
+        supportedSystems (system: self.packages."${system}".apexctl);
     };
 }
