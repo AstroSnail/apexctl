20426f91c09d930f7d28c096286d3241990deb17
AstroSnail <astrosnail@protonmail.com>
2020-09-23 20:43:23 +0000

    Use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,20 +1,18 @@
 {
   description = "A tool to control SteelSeries Apex keyboards on Linux";
-  outputs = { self, nixpkgs }:
-    let supportedSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
-    in {
-      packages = supportedSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          callApexctl = pkgs.callPackage self;
-        in {
-          apexctl = callApexctl { inherit self; };
-          apexctl-advanced = callApexctl {
-            inherit self;
-            advanced = true;
-          };
-        });
-      defaultPackage =
-        supportedSystems (system: self.packages.${system}.apexctl);
-    };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages = let
+        pkgs = nixpkgs.legacyPackages.${system};
+        callApexctl = pkgs.callPackage self;
+      in {
+        apexctl = callApexctl { inherit self; };
+        apexctl-advanced = callApexctl {
+          inherit self;
+          advanced = true;
+        };
+      };
+      defaultPackage = self.packages.${system}.apexctl;
+    });
 }
